607451dca7265197ee4a5b306677415e096379b3
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-18 12:37:35 -0400

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 
-(function(){  // the entire file is scope inside a anonymous function execution.
+(function(){
 
     $(document).ready(function(){
         setupOrientationHandler();
-
-        // hidden/commented in portfolio
-        $('.carousel').carousel(/* {interval:1000}*/)
-        
-        // Adjust active navbar title - yuck
-        var $t = $('title').html();        
-        if ($t.match(/Portfolio/)){
-            $('ul.nav li').removeClass('active');
-            $('ul.nav li a[href="/portfolio"]').parent().addClass('active');
-        }
-        if ($t.match(/Home/)){
-            $('ul.nav li').removeClass('active');
-            $('ul.nav li a[href="/home"]').parent().addClass('active');
-        }
     });
 
     function setupOrientationHandler(){
